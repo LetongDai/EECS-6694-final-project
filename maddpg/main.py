a435e1b7e8950b519ab9c98bbd57154008336f41
Letong Dai <dailetong@outlook.com>
2025-11-01 11:18:17 -0400

    add main.py

---
+++ maddpg/main.py
@@ -0,0 +1,15 @@
+from trainer import Trainer
+
+envs = None
+agents = None
+obs_size = 0
+act_size = 0
+
+epochs = 1000
+rollout_steps = 500
+batch_size = 16
+
+if __name__ == "__main__":
+  trainer = Trainer()
+  for i in range(epochs):
+    trainer.train_agents(self, rollout_steps, batch_size)
